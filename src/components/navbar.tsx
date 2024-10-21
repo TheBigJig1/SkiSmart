39bd920428135dec65c092547b0d638c5863a8e1
Mikus106 <msa00010@mix.wvu.edu>
2024-10-21 13:19:00 -0400

    Navbar Revision

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,10 +4,14 @@ import '@/styles/components/navbar.css'
     return (
     <div className='navbarContainer'>
         <div className='navLeft'>
+            
             <a href='/'> SkiSmart</a>
             
+            
         </div>
-        
+        <div className='imgContainer'>
+        <img src = "src\assets\logo.png" />
+        </div>
         <div className='navRight'><a href='/'>Home  </a>
         <a href='/temp1'>Resorts  </a>
         <a href='/temp2'>Feedback  </a>
