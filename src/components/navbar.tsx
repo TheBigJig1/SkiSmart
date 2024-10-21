3ff5fc2492203f22c3d7a995e66881158fdf1a6d
jmilkie01 <jmilkie22@gmail.com>
2024-10-21 11:10:20 -0400

    Tab Names
    Updated tab names

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,12 @@ import '@/styles/components/navbar.css'
  function Navbar() {
     return (
     <div className='navbarContainer'>
-        <a href='/'>First tab</a>
-        <a href='/temp1'>Second tab</a>
-        <a href='/temp2'>Third tab</a>
-        <a href='/temp3'>Fourth tab</a>
+        <a href='/'>Home</a>
+        <a href='/temp1'>Weather</a>
+        <a href='/temp2'>Resorts</a>
+        <a href='/temp3'>Feedback</a>
         <a href='/temp4'>Fifth tab</a>
-        <a href='/temp5'>Sixth tab</a>
+        <a href='/temp5'>Sign In</a>
     </div>
     )
  }
