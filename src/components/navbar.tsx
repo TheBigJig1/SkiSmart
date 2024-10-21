05319cf243cf809f06bda3e695071495d338c9be
jmilkie01 <jmilkie22@gmail.com>
2024-10-21 11:42:56 -0400

    Navbar
    Modified the nav bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,15 @@ import '@/styles/components/navbar.css'
  function Navbar() {
     return (
     <div className='navbarContainer'>
-        <a href='/'>Home</a>
-        <a href='/temp1'>Weather</a>
-        <a href='/temp2'>Resorts</a>
-        <a href='/temp3'>Feedback</a>
-        <a href='/temp4'>Fifth tab</a>
-        <a href='/temp5'>Sign In</a>
+        <div className='navLeft'>
+            <a href='/'>SkiSmart</a>
+        </div>
+        <div className='navRight'>
+            <a href='/'>Home</a>
+            <a href='/temp1'>Resorts</a>
+            <a href='/temp2'>Feedback</a>
+            <a href='/temp3'>Sign-in</a>
+        </div>
     </div>
     )
  }
