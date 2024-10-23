2e839d216bc8f584f07702cc44588085b1c8104d
Jaxon Fielding <129562525+TheBigJig1@users.noreply.github.com>
2024-10-23 12:59:02 -0400

    Background images working properly. Still need to address info below imagesn not moving up

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,9 +3,9 @@ import '@/styles/routes/home.css';
 function Home() {
     return (
         <div className="container">
-            <div className="backgroundImg">
-                <img src='src/assets/logoCircle.png' className="logo"></img> 
-                <div className="title">SkiSmart</div>
+            <div className="homeBackground">
+                <img className="homeLogo" src='src/assets/logoCircle.png'></img> 
+                <div className="homeTitle">SkiSmart</div>
             </div>
             <div className="textContainer">
                 Home page info
