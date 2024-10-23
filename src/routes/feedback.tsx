b90ba3d37ed54b60ba4eb65890a2a68cbd5e40a5
tpgirardi <girarditp@gmail.com>
2024-10-23 10:57:19 -0400

    Beginning Feedback
    I started adding feedback stuff

--- src/routes/feedback.tsx
+++ src/routes/feedback.tsx
@@ -1,9 +1,15 @@
+import '@/styles/routes/feedback.css';
 
 
 function Feedback() {
     return (
-    <div>
-        Feedback
+    <div className= "feedback-container">
+        <div className="feedback-box">
+            <label htmlFor="feedback">Feedback: </label>
+            <textarea id="feedback" rows={4} placeholder= "Type feedback here"> </textarea>
+
+        </div>
+
     </div>
     )
  }
