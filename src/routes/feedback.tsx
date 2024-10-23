79957d3db5e4e557c54e64453725ab9c4f2e06ad
tpgirardi <girarditp@gmail.com>
2024-10-23 18:04:01 -0400

    Feedback 1.2
    Added more to feedback page

--- src/routes/feedback.tsx
+++ src/routes/feedback.tsx
@@ -4,12 +4,15 @@ import mountain from '../assets/skiMountains.jpg';
 
 function Feedback() {
     return (
-
-        <div className="backImg" style={{ backgroundImage: `url(${mountain})` }}>
-            <div className= "feedback-container">
-                <div className="feedback-box">
-                    <label htmlFor="feedback">Feedback: </label>
-                    <textarea id="feedback" rows={4} placeholder= "Type feedback here"> </textarea>
+        <div className="container">
+            <div className="backImg" style={{ backgroundImage: `url(${mountain})` }}>
+                <img src='src/assets/logoCircle.png' className="logo"></img>
+                <div className="title">SkiSmart</div>
+                <div className= "feedback-container">
+                    <div className="feedback-box">
+                        <label htmlFor="feedback">Feedback: </label>
+                        <textarea id="feedback" rows={4} placeholder= "Type feedback here"> </textarea>
+                    </div>
                 </div>
             </div>
         </div>
