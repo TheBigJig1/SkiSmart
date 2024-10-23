e5836bcd424b8c60499f15bfa08d7dabe345fe1a
tpgirardi <girarditp@gmail.com>
2024-10-23 11:46:16 -0400

    Feedback 1.1
    Made it look a lot better.

--- src/routes/feedback.tsx
+++ src/routes/feedback.tsx
@@ -1,16 +1,19 @@
 import '@/styles/routes/feedback.css';
+import mountain from '../assets/skiMountains.jpg';
 
 
 function Feedback() {
     return (
-    <div className= "feedback-container">
-        <div className="feedback-box">
-            <label htmlFor="feedback">Feedback: </label>
-            <textarea id="feedback" rows={4} placeholder= "Type feedback here"> </textarea>
 
+        <div className="backImg" style={{ backgroundImage: `url(${mountain})` }}>
+            <div className= "feedback-container">
+                <div className="feedback-box">
+                    <label htmlFor="feedback">Feedback: </label>
+                    <textarea id="feedback" rows={4} placeholder= "Type feedback here"> </textarea>
+                </div>
+            </div>
         </div>
 
-    </div>
     )
  }
 
