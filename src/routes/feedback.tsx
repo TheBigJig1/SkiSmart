302a1468ca02a00cc5ee6cc91c741ef8ca8f4d1a
tpgirardi <girarditp@gmail.com>
2024-10-24 21:48:07 -0400

    Feedback 1.4
    Added review portion with stars and submit button to feedback. Still needs to be properly set up.

--- src/routes/feedback.tsx
+++ src/routes/feedback.tsx
@@ -3,21 +3,43 @@ import mountain from '../assets/skiMountains.jpg';
 
 
 function Feedback() {
+    const hanStarCl= () =>
+    {
+        console.log('Star clicked');
+    };
+    const handleSub= () =>
+    {
+        console.log('Feedback submitted');
+
+    }
     return (
         <div className="fcontainer">
             <div className="backImg" style={{ backgroundImage: `url(${mountain})` }}>
                 <img src='src/assets/logoCircle.png' className="flogo"></img>
                 <div className="ftitle">SkiSmart</div>
                 <div className= "feedback-container">
-                    <div className="feedback-box">
+                    <form className="feedback-box" onSubmit={handleSub}>
                         <label htmlFor="feedback">Feedback: </label>
                         <textarea id="feedback" rows={4} placeholder= "Type feedback here"> </textarea>
-                    </div>
+                        <h3> Leave Us A Review:</h3>
+                        <div className="starRating">
+                            {[...Array(5)].map((_,index)=> (
+                                <span 
+                                    key= {index} 
+                                    className="star" 
+                                    onClick={()=> hanStarCl()}
+                                    >
+                                         ★
+                                    </span>
+                             ))}
+                        </div>
+                        <button type= "submit"> Submit </button>
+                    </form>
                 </div>
             </div>
         </div>
 
-    )
+    );
  }
 
  export default Feedback
