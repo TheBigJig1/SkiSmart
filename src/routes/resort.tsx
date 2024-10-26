3517f9d7f988084b2fc510052b0a51b92aac7243
Jaxon Fielding <129562525+TheBigJig1@users.noreply.github.com>
2024-10-26 18:44:01 -0400

    updated resorts page. Kind of works, want to improve. Grid for main page content is the move

--- src/routes/resort.tsx
+++ src/routes/resort.tsx
@@ -2,12 +2,12 @@ import "@/styles/routes/resort.css"
 import ResortCard from "../components/resortCard"
 function Resort() {
     return (
-    <div className="container">
-        <div className="backgroundImg">
-            <img src='src/assets/logoCircle.png' className="logo"></img>
-            <div className="title">SkiSmart</div>
+    <div className="landingContainer">
+        <div className="landingBackground">
+            <img src='src/assets/logoCircle.png' className="landingLogo"></img>
+            <div className="landingTitle">SkiSmart</div>
         </div>
-        <div className="resortsCardContainer">
+        <div className="textContainer">
             <ResortCard resortName="Timberline" snowfall="3 in." address="254 Four Seasons Dr, Davis, WV 26260"/>
             <ResortCard resortName="The Wisp" snowfall="1.4 in." address="296 Marsh Hill Rd, McHenry, MD 21541"/>
         </div>
