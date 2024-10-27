53bf378e7e6ace8ce69d075397fdb5e56167c667
Jaxon Fielding <129562525+TheBigJig1@users.noreply.github.com>
2024-10-27 16:14:31 -0400

    Made final changes to resort

--- src/routes/resort.tsx
+++ src/routes/resort.tsx
@@ -1,13 +1,14 @@
 import "@/styles/routes/resort.css"
 import ResortCard from "../components/resortCard"
+
 function Resort() {
     return (
-    <div className="pageContainer">
-        <div className="pageBackground">
-            <img src='src/assets/logoCircle.png' className="logo"></img>
-            <div className="title">SkiSmart</div>
+    <div className="resortContainer">
+        <div className="resortBackground">
+            <img src='src/assets/logoCircle.png' className="resortLogo"></img>
+            <div className="resortTitle">SkiSmart</div>
         </div>
-        <div className="contentContainer">
+        <div className="resortContentContainer">
             <ResortCard resortName="Timberline" snowfall="3 in." address="254 Four Seasons Dr, Davis, WV 26260"/>
             <ResortCard resortName="The Wisp" snowfall="1.4 in." address="296 Marsh Hill Rd, McHenry, MD 21541"/>
         </div>
