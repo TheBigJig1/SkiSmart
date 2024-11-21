46ff7bcf38b88dec5d5e9dcb5f6cd9183c7507c4
Jaxon Fielding <129562525+TheBigJig1@users.noreply.github.com>
2024-11-20 19:45:46 -0500

    Submit feedback works

--- src/routes/feedback.tsx
+++ src/routes/feedback.tsx
@@ -1,23 +1,72 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import '@/styles/routes/feedback.css';
+import { jwtDecode }  from 'jwt-decode';
+
 
 function Feedback() {
     const [selectRate, setSelectRate]=useState(0);
     const [feedback,setFeedback]=useState('');
+    const [email, setEmail] = useState('');
+    
+    useEffect(() => {
+        // Retrieve user data from localStorage
+        const token = localStorage.getItem('token') || ''
+        if (!token) {
+            setEmail('AnonymousCoward');
+            return;
+        }
+        const decoded = jwtDecode(token) as { user: { email: string; first: string; last: string; zipcode: string } };
+        const user = decoded.user;
+        if (user && user.email) {
+            setEmail(user.email);
+        }
+    }, []);
 
     const hanStarCl= (index: number) =>
     {
         setSelectRate(index+1);
         console.log('Star clicked');
     };
-    const handleSub= () =>
+
+    const handleSub = async (e: React.FormEvent<HTMLFormElement>) =>
     {
-        //We'll just need to add a submit part that actually submits the feedback and ratings before reseting
+        e.preventDefault();
+
+        const formData = new URLSearchParams();
+        formData.append('email', email);
+        formData.append('feedback', feedback);
+        formData.append('rating', selectRate.toString());
+
+        try {
+            const response = await fetch('http://localhost:8080/feedback/add', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: formData.toString(),
+                // credentials: 'include',
+              });
+
+              if (response.ok) {
+                // Handle successful response
+                console.log('Feedback submitted successfully');
+                alert('Feedback submitted successfully!');
+        
+              } else {
+                // Handle error response
+                console.error('Error submitting feedback');
+              }
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+
+        // We'll just need to add a submit part that actually submits the feedback and ratings before reseting
         console.log('Feedback submitted', feedback,selectRate);
         setFeedback(''); //resets feedback
         setSelectRate(0); //resets star rating
-
     };
+
     return (
         <div className="fcontainer">
             <div className="fbackground">
